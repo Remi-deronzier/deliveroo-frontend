ea4bff58c269d8f74a6e164fa2538f2d0ec2b1ad
Remi-deronzier <deronzier.remi@gmail.com>
2021-07-14 17:05:26 +0200

    responsive

--- src/Components/Restaurant.js
+++ src/Components/Restaurant.js
@@ -3,15 +3,17 @@ import "./Restaurant.css";
 const Restaurant = ({ data }) => {
   return (
     <div className="restaurant">
-      <div className="container restaurant-content">
-        <div className="call1">
-          <h1>{data.restaurant.name}</h1>
-          <p className="restaurant-description">
-            {data.restaurant.description}
-          </p>
-        </div>
-        <div className="call2">
-          <img src={data.restaurant.picture} alt={data.restaurant.name} />
+      <div className="container">
+        <div className="restaurant-content">
+          <div className="call1">
+            <h1>{data.restaurant.name}</h1>
+            <p className="restaurant-description">
+              {data.restaurant.description}
+            </p>
+          </div>
+          <div className="call2">
+            <img src={data.restaurant.picture} alt={data.restaurant.name} />
+          </div>
         </div>
       </div>
     </div>
